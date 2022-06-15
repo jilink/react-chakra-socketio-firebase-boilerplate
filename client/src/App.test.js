fec568b6793b50f10ac9af895951fd90b970e4aa
jilink <jimmyfac@gmail.com>
2022-06-15 14:44:05 +0200

    fixing test bug

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,8 +3,22 @@ import { screen } from '@testing-library/react';
 import { render } from './test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test', () => {
+  beforeAll(() => {
+    window.matchMedia = query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // Deprecated
+      removeListener: jest.fn(), // Deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    });
+  });
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn chakra/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
